c8c3f2d2de77d9e34ee2271d53bfc44c19e46f36
Marcos Douglas <88040554+MardouSilva@users.noreply.github.com>
2024-04-26 13:25:11 -0300

    Initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+import { NavLink } from './nav-link';
+
+export function Header(){
+    return (
+        <header className='flex items-center gap-5 py-2'>
+            <img src={nlwUniteIcon} alt="Icone" />
+            <nav className='flex items-center gap-5'>
+                <NavLink href="#">Eventos</NavLink>
+                <NavLink href="#">Participantes</NavLink>
+            </nav>
+        </header>
+    )
+}
